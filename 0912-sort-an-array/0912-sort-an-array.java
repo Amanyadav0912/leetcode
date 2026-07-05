f708c4efe3064ad08b80cf02eaf1b92ab2337d7a
Aman Yadav <amanjy0707@gmail.com>
2026-07-06 01:55:26 +0530

    Time: 98 ms (12.53%), Space: 81.1 MB (84.06%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer>pq=new
+        PriorityQueue<>();
+        for(int i:nums)
+        {
+            pq.add(i);
+        }
+
+        int ans[]=new int[nums.length];
+        for(int i=0; i<nums.length;i++)
+        {
+            ans[i]=pq.peek();
+            pq.poll();
+        }
+        return ans;
+        
+
+        
+        
+    }
+}
